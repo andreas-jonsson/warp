eedc0c00838b6568fd25244a6eb30bbed9204116
Andreas T Jonsson <mail@andreasjonsson.se>
2016-11-04 11:11:49 +0100

    Added nanovgo.

---
+++ warp.go
@@ -0,0 +1,96 @@
+/*
+Copyright (C) 2016 Andreas T Jonsson
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/andreas-jonsson/nanovgo"
+	"github.com/go-gl/gl/v2.1/gl"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+func main() {
+	var winTitle string = "Go-SDL2 + Go-GL"
+	var winWidth, winHeight int = 800, 600
+	var window *sdl.Window
+	var context sdl.GLContext
+	var event sdl.Event
+	var running bool
+	var err error
+
+	if err = sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+
+	if err = gl.Init(); err != nil {
+		panic(err)
+	}
+
+	window, err = sdl.CreateWindow(winTitle, sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		winWidth, winHeight, sdl.WINDOW_OPENGL)
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+	context, err = sdl.GL_CreateContext(window)
+	if err != nil {
+		panic(err)
+	}
+	defer sdl.GL_DeleteContext(context)
+
+	gl.Enable(gl.DEPTH_TEST)
+	gl.ClearColor(0.2, 0.2, 0.3, 1.0)
+	gl.ClearDepth(1)
+	gl.DepthFunc(gl.LEQUAL)
+	gl.Viewport(0, 0, int32(winWidth), int32(winHeight))
+
+	ctx, err := nanovgo.NewContext(0 /*nanovgo.AntiAlias | nanovgo.StencilStrokes | nanovgo.Debug*/)
+	if err != nil {
+		panic(err)
+	}
+	defer ctx.Delete()
+
+	running = true
+	for running {
+		for event = sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch t := event.(type) {
+			case *sdl.QuitEvent:
+				running = false
+			case *sdl.MouseMotionEvent:
+				fmt.Printf("[%d ms] MouseMotion\tid:%d\tx:%d\ty:%d\txrel:%d\tyrel:%d\n", t.Timestamp, t.Which, t.X, t.Y, t.XRel, t.YRel)
+			}
+		}
+		drawgl()
+		sdl.GL_SwapWindow(window)
+	}
+}
+
+func drawgl() {
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+	gl.Begin(gl.TRIANGLES)
+	gl.Color3f(1.0, 0.0, 0.0)
+	gl.Vertex2f(0.5, 0.0)
+	gl.Color3f(0.0, 1.0, 0.0)
+	gl.Vertex2f(-0.5, -0.5)
+	gl.Color3f(0.0, 0.0, 1.0)
+	gl.Vertex2f(-0.5, 0.5)
+	gl.End()
+}
